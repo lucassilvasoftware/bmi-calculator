acdc1a4daac0c78d5efe2ddbd8197330e326ac0e
lucassilvasoftware <54152996+lucassilvasoftware@users.noreply.github.com>
2021-12-28 21:04:54 -0300

    implemesharing system implemented

--- src/components/Form/ResultBMI/index.js
+++ src/components/Form/ResultBMI/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Share } from 'react-native';
 import styles from './style';
 
 export default function ResultBMI(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: "My BMI today is: " + props.resultBMI,
+    })
+  }
+
   return (
     <View style={styles.contextBMI}>
+      <View>
+        {props.resultBMI != null ?
+          <TouchableOpacity>
+            <Text>Share</Text>
+          </TouchableOpacity>
+          :
+          <View />
+        }
+      </View>
       <Text style={styles.titleResultIMC}>{props.messageResultBMI}</Text>
       <Text style={styles.resultBMI}>{props.resultBMI}</Text>
-    </View>
+    </View >
   );
 }
